f7fe64e70e026bf7d1dd8ac8e3a42360e116c3d3
krishan041 <krishandara04@gmail.com>
2025-01-21 20:20:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,9 +7,9 @@ chmod +x start.sh
 # source venv/bin/activate
 
 # Export environment variables if needed
-# export BOT_TOKEN="your_bot_token"
-# export API_ID="your_api_id"
-# export API_HASH="your_api_hash"
+# export BOT_TOKEN=""
+# export API_ID="27536109"
+# export API_HASH="b84d7d4dfa33904d36b85e1ead16bd63"
 
 # Start the bot
 python3 main.py
