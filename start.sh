e4fbb2bb4a4ed2399b5e17e00c3c818fce3e2bdf
krishan041 <krishandara04@gmail.com>
2025-01-21 20:04:07 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Make the script executable
+chmod +x start.sh
+
+# Activate virtual environment if needed
+# source venv/bin/activate
+
+# Export environment variables if needed
+# export BOT_TOKEN="your_bot_token"
+# export API_ID="your_api_id"
+# export API_HASH="your_api_hash"
+
+# Start the bot
+python3 main.py
