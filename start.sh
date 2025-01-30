a99eb5649f8858cf893b784b5c88d75f94dc01bb
krishan041 <krishandara04@gmail.com>
2025-01-30 20:29:29 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-# Make the script executable
-chmod +x start.sh
-
-# Activate virtual environment if needed
-# source venv/bin/activate
-
-# Export environment variables if needed
-# export BOT_TOKEN=""
-# export API_ID="27536109"
-# export API_HASH="b84d7d4dfa33904d36b85e1ead16bd63"
-
-# Start the bot
-python3 main.py
+python3 update.py && python3 -m bot
